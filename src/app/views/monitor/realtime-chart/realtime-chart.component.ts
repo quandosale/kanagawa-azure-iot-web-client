b5d96ee0864308a22ea5078c03c0512afe1b578c
nkhs <nkhs@yandex.com>
2018-03-26 11:35:55 +0800

    update recording duration error

--- src/app/views/monitor/realtime-chart/realtime-chart.component.ts
+++ src/app/views/monitor/realtime-chart/realtime-chart.component.ts
@@ -149,27 +149,34 @@ export class RealtimeChartComponent implements OnInit, OnDestroy {
           console.log(res);
         })
     } else {
+
       if (this.recordDataset == null) {
         console.log('cancel Record')
         this.dataService.cancelRecord(this.device._id, '')
           .subscribe(res => {
             console.log(res);
+
           })
         return;
       }
-
+      
       this.recordDataset.duration = new Date().getTime() - this.recordDataset.start;
       if (this.recordDataset.duration < 10 * 1000) {
         console.log('less than 10seconds, cancel Record');
+        
         this.dataService.cancelRecord(this.device._id, this.recordDataset.datasetId)
           .subscribe(res => {
             console.log(res);
+            this.recordDataset = null;
+            this.sharedService.setRecordingStatus(this.device._id, null);
           })
         return;
       }
       this.dataService.stopRecord(this.recordDataset)
         .subscribe(res => {
           console.log(res);
+          this.recordDataset = null;
+          this.sharedService.setRecordingStatus(this.device._id, null);
         })
     }
   }
